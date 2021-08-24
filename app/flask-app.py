4f08f0a68bb9659d138c8b4368c55a19b629338c
xmateq <mati.krzesiak@gmail.com>
2021-08-24 10:46:48 +0200

    app style fixes

--- app/flask-app.py
+++ app/flask-app.py
@@ -3,20 +3,23 @@ import socket
 import os
 
 app = Flask(__name__)
-  
+
+
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
     return hostname
 
+
 @app.route('/healthz')
 def health():
     return "healthy"
+
 
 @app.route('/ready')
 def ready():
     return "ready"
 
-  
+
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = os.environ.get("PORT"), debug = os.environ.get("DEBUG"))
+    app.run(host='0.0.0.0', port=os.environ.get("PORT"), debug=os.environ.get("DEBUG"))
